3a81fa9082c8901c5008ddd67d3a4a059fe57e11
Dark Killer <86813581+ImDarkLK@users.noreply.github.com>
2022-04-04 20:22:25 +0530

    Release v1.0

---
+++ xray.sh
@@ -0,0 +1,11 @@
+apt-get update
+
+apt-get upgrade -y
+
+sudo git clone https://github.com/iamtrazy/bash-xray-script
+
+cd bash-xray-script
+
+sudo chmod 777 xray-nodomain.sh
+
+sudo ./xray-nodomain.sh
